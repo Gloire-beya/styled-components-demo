6c88243e2dad2271a29a406e3b70609b57861f09
Gloire Beya <gloire.beya@xds.co.za>
2024-02-06 16:56:09 +0200

    Basics completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container } from './compoents/styles/Container.styled'
+import Header from './compoents/Header';
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './compoents/styles/Global';
+
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333'
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Header />
+        <Container>
+          <h1>Hi there </h1>
+        </Container>
+      </>
+    </ThemeProvider>
+  )
 }
 
 export default App;
